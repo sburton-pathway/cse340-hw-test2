942882048c24315d10488c154ab41aa1c727db1a
sburton42 <scotthburton@gmail.com>
2025-12-16 16:23:22 -0700

    Week 03 learning activities are now complete. This is the addition of the organization details page.

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,13 +1,23 @@
 // Import any needed model functions
-import { getAllOrganizations } from '../models/organizations.js';
+import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
+import { getProjectsByOrganizationId } from '../models/projects.js';
 
 // Define any controller functions
 const partnersPage = async (req, res) => {
     const organizations = await getAllOrganizations();
     const title = 'Our Partners';
-    
+
     res.render('partners', { title, organizations });
 };
 
+const organizationDetailsPage = async (req, res) => {
+    const organizationId = req.params.id;
+    const organizationDetails = await getOrganizationDetails(organizationId);
+    const projects = await getProjectsByOrganizationId(organizationId);
+    const title = 'Organization Details';
+
+    res.render('organizationDetails', {title, organizationDetails, projects});
+};
+
 // Export any controller functions
-export { partnersPage };
+export { partnersPage, organizationDetailsPage };
--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -5,6 +5,7 @@ import { partnersPage } from './organizations.js';
 import { projectsPage } from './projects.js';
 import { categoriesPage } from './categories.js';
 import { testErrorPage } from './errors.js';
+import { organizationDetailsPage } from './organizations.js';
 
 const router = express.Router();
 
@@ -12,6 +13,9 @@ router.get('/', homePage);
 router.get('/partners', partnersPage);
 router.get('/projects', projectsPage);
 router.get('/categories', categoriesPage);
+
+// Route for organization details page
+router.get('/organization/:id', organizationDetailsPage);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
